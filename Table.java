08fa1f8b92ebe41404badb2a6a95a42ea1c18dce
abishekn1 <abishekraj0405@gmail.com>
2023-09-15 16:45:04 +0530

    array programs

---
+++ Table.java
@@ -0,0 +1,16 @@
+public class Table{
+
+	public static void main(String[] args){
+
+		byte[] marks = {60, 80, 68 , 10, 20 , 33, 44, 85 , 90};
+
+		System.out.println(marks[1]);
+
+		for(int i = marks.length-1 ; i >=0; i--){
+			
+			System.out.println(marks[table]);
+		}
+
+	}
+	
+}
